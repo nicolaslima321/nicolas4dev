04b254a657fadf475c2064167f2b8741eaf8df8f
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-03-14 21:22:18 -0300

    added sass processor at config, and also icons plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
 import { defineNuxtConfig } from 'nuxt3'
+import UnpluginComponentsVite from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  buildModules: ['unplugin-icons/nuxt'],
+  modules: ['@nuxtjs/color-mode'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/main.scss";',
+        },
+      },
+    },
+    plugins: [
+      Icons({ autoInstall: true }),
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+    ],
+  },
 })
