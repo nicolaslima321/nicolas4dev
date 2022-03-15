e624d6b532b66fe7cf8ced2ca2ee8b4ad8da0277
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-03-14 21:23:50 -0300

    created first page with dark mode

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+const colorMode = useColorMode();
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300&display=swap');
+
+html {
+  font-family: 'Fredoka', sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <img
+      class="index__img"
+      height="64px"
+      width="64px"
+      :src="picture"
+    />
+    teste <IconUil:linkedin /> testeee
+    <div>
+      <h1>Color mode: {{ $colorMode.value }}</h1>
+      <select v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="sepia">Sepia</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import picture from '@/assets/images/main.jpeg';
+
+export default defineComponent({
+  setup() {
+    console.log('hellowww');
+
+    return {
+      picture
+    }
+  },
+})
+</script>
+
+
+<style lang="scss">
+.index__img {
+  border-radius: 50%;
+}
+
+span {
+  color: #FA56BA
+}
+</style>
